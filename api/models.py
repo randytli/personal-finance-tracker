839ad2200355a0448ccb29319bb470238fe89941
randyli <landelitianrun@berkeley.edu>
2025-08-01 21:00:53 +0000

    test updated git config

--- api/models.py
+++ api/models.py
@@ -8,3 +8,4 @@ class Item(Base):
     item_id = Column(String, primary_key=True)
     access_token = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
